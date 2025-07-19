2fe9949303b97e78bf42f412e0d9e216c7e19b49
teelol456 <teerapong.ni@ku.th>
2025-07-20 01:58:47 +0700

    Add files via upload

---
+++ integerSetTest.java
@@ -0,0 +1,10 @@
+public class integerSetTest {
+    public static void main(String[] args) {
+        integerSet a = new integerSet();
+        a.add(1);
+        a.add(2);
+        a.add(4);
+        a.add(3);
+        System.out.println(a.toString());
+    }
+}
